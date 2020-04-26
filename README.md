0a64dcdcfb2db2bbb7a8634d0df127d863ad9a4c
jeenut27 <jeenuthomas27@gmail.com>
2020-04-26 21:10:29 +0530

    Adding code updates

--- README.md
+++ README.md
@@ -2,7 +2,119 @@
 
 Python library to use the SurveyCTO API to download data
 
-## API Options
+
+# Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Use Cases](#usecases)
+* [License](#license)
+* [SCTO API Options](#apioptions)
+
+
+<a name="installation"></a>
+# Installation
+
+## Prerequisites
+
+- Python version 2.6, 2.7, 3.4, 3.5 or 3.6
+
+## Install Package
+```bash
+pip install pysurveycto
+```
+
+
+<a name="usage"></a>
+# Usage
+
+```python
+SurveyCTOObject(server_name, username, password, keyfile=False)
+```
+Initialize SCTO Object
+Parameters:
+- server_name (str): SurveyCTO server name
+- username (str): SurveyCTO login username
+- password (str): SurveyCTO login password
+- keyfile (str, optional)
+
+```python
+get_form_data(form_id, format=’csv’, shape=’wide’, date=None, review_status='approved', repeat_groups=None, line_breaks=None)
+```
+Fetch SurveyCTO form data in json or csv formats
+Parameters
+- form_id (str): The form_id of the SurveyCTO form.
+- format (str, optional): The format of the returned data. Allowed values are: json, csv(default).
+- shape (str, optional): The shape of the returned data. Allowed values are: wide, long. shape=’long’ can only be specified when returning data in csv format.
+- date (str, optional): Return only the form submissions where CompletionDate is greater than the given date (in UTC). Can only be specified when returning data in json format.
+- review_status (str, optional): Return only the form submissions with given review status. Allowed values are: approved(default), rejected, pending and more than one value concatenated with | or with commas.
+- repeat_groups (bool, optional): Return a dictionary object containing the main form data along with the repeat groups. Can only be specified when returning long data, in which case it will default to true.
+- line_breaks (str, optional): Replace linebreaks in the csv data with some other character.
+
+```python
+get_repeatgroup(form_id, repeat_group_name, review_status='approved', line_breaks=None)
+```
+Fetch SurveyCTO form's repeatgroup data
+Parameters
+- form_id (str): The form_id of the SurveyCTO form.
+- repeat_group_name (str): Form's repeat group name.
+- review_status (str, optional): Return only the form submissions with given review status. Allowed values are: approved(default), rejected, pending and more than one value concatenated with | or with commas.
+- line_breaks (str, optional): Replace linebreaks in the csv data with some other character.
+
+```python
+get_server_dataset(dataset_id, line_breaks=None)
+```
+Fetch SurveyCTO server dataset data
+Parameters
+- dataset_id (str): The server dataset id of the SurveyCTO dataset
+- line_breaks (str, optional): Replace linebreaks in the csv data with some other character.
+
+```python
+get_attachment(url)
+```
+Fetch form's file attachments like media/audio/images from SurveyCTO
+Parameters
+- url (str): The URL to fetch the attached file
+
+
+<a name="usecases"></a>
+# Use Cases
+
+- Get a wide csv
+```python
+get_form_data(form_id)
+```
+
+# Get a wide json
+```python
+get_form_data(form_id, format=’json’)
+```
+
+# Get a long csv with repeat groups
+```python
+get_form_data(form_id, shape=’long’)
+```
+
+# Get a long csv without repeat groups
+```python
+get_form_data(form_id, shape=’long’, repeat_groups=false)
+```
+
+# Get a wide json starting after a given CompletionDate
+```python
+get_form_data(form_id, format=’json’, date=my_datetime)
+```
+
+
+<a name="license"></a>
+# License
+[The MIT License (MIT)](LICENSE.md)
+
+
+<a name="apioptions"></a>
+# SCTO API Options
+
+[SCTO API Documentation](https://support.surveycto.com/hc/en-us/articles/360033156894?flash_digest=0a6eded7694409181788cc46a7026897850d65b5&flash_digest=d76dde7c3ffc40f4a7f0ebd87596d32f3a52304f)
 
 * JSON format: https://servername.surveycto.com/api/v2/forms/data/wide/json/formid?date=
   - servername
--- pysurveycto/pysurveycto.py
+++ pysurveycto/pysurveycto.py
@@ -1,61 +1,107 @@
 import requests
+import urllib
+import datetime
+
+class IllegalArgumentError(ValueError):
+	pass
 
 class SurveyCTOObject(object):
-    """
+	"""
 
-    """
+	"""
 
-    def __init__(self,
-    			 server_name,
-    			 username,
-    			 password,
-    			 keyfile=False):
-
-    """
-    Function to form the basic url
-
-    """
-    
-    self.server_name = server_name
-
-    # Defining both to be compatible with all SCTO versions
-   	self.auth_basic = requests.auth.HTTPBasicAuth(username, 
-									  	   		  password)
-   	self.auth_disgest = requests.auth.HTTPDigestAuth(username, 
-									  	   		     password)
-
-   	self.keyfile = keyfile
-
-   	# Only header we are using -- Check if any more should be provided as options
- 	self.default_headers = {
-					    	'X-OpenRosa-Version': '1.0',
-					  	   }
-
-
-
-	def get_data_from_url(self,
-						  url):
-
+	def __init__(self,
+				 server_name,
+				 username,
+				 password,
+				 keyfile=False):
 		"""
-			Function to fetch data directly from a scto url
-	        :param url: SurveyCTO URL
+			Initialize SCTO Object
+			:param server_name (str): SurveyCTO server name
+			:param username (str): SurveyCTO login username
+			:param password (str): SurveyCTO login password
+			:param keyfile (str, optional)
+			
 		"""
+
+		self.server_name = server_name
+
+		# Defining both to be compatible with all SCTO versions
+		self.auth_basic = requests.auth.HTTPBasicAuth(username,
+													  password)
+		self.auth_disgest = requests.auth.HTTPDigestAuth(username,
+														 password)
+
+		self.keyfile = keyfile
+
+		# Only header we are using -- Check if any more should be provided as options
+		self.default_headers = {
+			'X-OpenRosa-Version': '1.0',
+		}
+
+	def get_url_data(self,
+					 url,
+					 line_breaks=None):
+		"""
+				Function to fetch data directly from a scto url
+				:param url: SurveyCTO URL
+		"""
+
+		# Change line break settings as per user parameter
+		if (line_breaks is not None):
+			v_url_encoded_line_break = urllib.parse.quote(line_breaks)
+			v_settings = f'''https://{self.server_name}.surveycto.com/api/v1/forms/settings/csv/linebreak?v={v_url_encoded_line_break}'''
+			try:
+				response = requests.post(
+					v_settings, headers=self.default_headers, auth=self.auth_basic)
+				response.raise_for_status()
+			except Exception as e:
+				try:
+					response = requests.post(
+						v_settings, headers=self.default_headers, auth=self.auth_disgest)
+					response.raise_for_status()
+				except Exception as e:
+					response = False
+					raise ValueError(e)
+		else:
+			# restore default
+			v_settings = f'https://{self.server_name}.surveycto.com/api/v1/forms/settings/csv/linebreak'
+
+			try:
+				response = requests.delete(
+					v_settings, headers=self.default_headers, auth=self.auth_basic)
+				response.raise_for_status()
+			except Exception as e:
+				try:
+					response = requests.delete(
+						v_settings, headers=self.default_headers, auth=self.auth_disgest)
+					response.raise_for_status()
+
+				except Exception as e:
+
+					response = False
+					raise ValueError(e)
+
+		
 		try:
 			if self.keyfile is False:
-				response = requests.get(url, headers=self.default_headers, auth=self.auth_basic)
+				response = requests.get(
+					url, headers=self.default_headers, auth=self.auth_basic)
 			else:
 				files = {'private_key': open(self.keyfile, 'rb')}
-				response = requests.post(url, files=files, headers=self,default_headers, auth=self.auth_basic)
+				response = requests.post(url, files=files, headers=self.default_headers, auth=self.auth_basic)
 
 			response.raise_for_status()
-			
+
 		except Exception as e:
 			try:
 				if self.keyfile is False:
-					response = requests.get(url, headers=self.default_headers, auth=self.auth_disgest)	
+					response = requests.get(
+						url, headers=self.default_headers, auth=self.auth_disgest)
 				else:
 					files = {'private_key': open(self.keyfile, 'rb')}
-					response = requests.post(url, files=files, headers=self.default_headers, auth=self.auth_disgest)
+					response = requests.post(
+						url, files=files, headers=self.default_headers, auth=self.auth_disgest)
 
 				response.raise_for_status()
 
@@ -66,71 +112,159 @@ class SurveyCTOObject(object):
 		return response
 
 
-
 	def get_form_data(self,
 					  form_id,
-					  long_format=0):	
-
+					  format='csv',
+					  shape='wide',
+					  date=None,
+					  review_status='approved',
+					  repeat_groups=None,
+					  line_breaks=None):
 		"""
-			Function to get form data in csv format
-			:param form_id: SurveyCTO's unique identifier for the form
-			:param long_format: Set to 1 if data to be downloaded in long format, default is wide
+				Function to get form data in csv format
+				:param form_id: SurveyCTO's unique identifier for the form
+				:param format: Format of the returned data. Options are csv (default) and json
+				:param shape: Shape of the returned data. Options are wide (default) and long
+										  Shape='long' can be specified only when format of returned data is csv
+				:param date: Date for which the data is to be downloaded, default 0 means all dates
+										 Date can only be specified when format of returned data is json
+				:param review_status: Review status for the data to be downloaded, Options are approved(default), rejected and pending
+									  Can also accept more than one value, concatenated with | or with commas
+				:param repeat_groups: Boolean set to true to return a dictionary with all repeat groups. 
+														  Can be specified only when shape is long in which case it default to true
+				:param line_breaks: Option to replace linebreaks in the csv data with some other character
 		"""
 
-		if (long_format == 0):
-			url = f'https://{self.server_name}.surveycto.com/api/v1/forms/data/wide/csv/{form_id}'
-			data = (get_data_from_url(self, url)).text
-			return data
+		review_status_arr = review_status.replace('|', ',').split(',')
+
+		for status in review_status_arr:
+			if (review_status not in ['approved', 'pending', 'rejected']):
+				raise IllegalArgumentError(
+					"Wrong value passed in review_status. Allowed values are approved, rejected and pending.")
+			else:
+				url_review_status=review_status
+
+		if (format == 'csv'):
+
+			# check params
+			if (date is not None):
+				raise IllegalArgumentError(
+					"Date can only be specified when extracting data in json format")
+
+			if (shape not in ['long', 'wide']):
+				raise IllegalArgumentError(
+					"Wrong value passed in shape. Allowed values are long and wide")
+
+			if (shape == 'wide'):
+
+				if (repeat_groups is not None):
+					raise IllegalArgumentError(
+						"Repeat groups can only be specified when extracting data in csv long format")
+
+				url = f'https://{self.server_name}.surveycto.com/api/v1/forms/data/wide/csv/{form_id}?r={url_review_status}'
+				data = (self.get_url_data(url, line_breaks)).text
+				return data
+			else:
+
+				if (repeat_groups == False):
+
+					url = f'https://{self.server_name}.surveycto.com/api/v1/forms/data/csv/{form_id}?r={url_review_status}'
+					data = (self.get_url_data(url, line_breaks)).text
+					return data
+
+				else:
+
+					# Default to returning all repeat groups in a distionary
+					files_url = f'https://{self.server_name}.surveycto.com/api/v1/forms/files/csv/{form_id}'
+					url_list = (self.get_url_data(files_url, line_breaks)).text
+					data_dict = {}
+					url_count = 0
+					for url in url_list.splitlines():
+						url = url + f'?r={url_review_status}'
+						data = (self.get_url_data(url, line_breaks)).text
+						data_dict[url_count] = data
+						url_count = url_count + 1
+
+					return data_dict
+
 		else:
-			# Get list of files and then get data for each file - Should the data be formatted as a dictionary
-			print("to be written")
 
+			if (shape != 'wide'):
+				raise IllegalArgumentError(
+					"Shape can only be specified when extracting data in csv format")
 
+			if (repeat_groups is not None):
+				raise IllegalArgumentError(
+					"Repeat groups can only be specified when extracting data in csv long format")
 
-	def get_form_data_as_json(self,
-					  	   	  form_id,
-					  	      date=0):	
+			if (line_breaks is not None):
+				raise IllegalArgumentError(
+					"Line breaks can only be specified when extracting data in csv format")
 
+			if ((date == 0) or (date is None)):
+				# Default to fecthing data for all dates
+				url_date = 0
+			else:
+				if (isinstance(date, datetime.date) or isinstance(date, datetime.datetime)):
+
+					if (isinstance(date, datetime.date)):
+						# convert date to required format
+						date = datetime.datetime.combine(
+							date, datetime.datetime.min.time())
+
+					url_date = urllib.parse.quote(
+						date.strftime("%b %-d, %Y %-I:%M:%S %p"))
+
+				else:
+					raise TypeError(
+						'Date arg must be a datetime.date or datetime.datetime object')
+
+			url = f'https://{self.server_name}.surveycto.com/api/v2/forms/data/wide/json/{form_id}?date={url_date}?r={url_review_status}'
+			data = (self.get_url_data(url)).json()
+			return data
+
+	def get_repeatgroup(self,
+						form_id,
+						repeat_group_name,
+						review_status='approved'):
 		"""
-			Function to get form data in json format
-			:param form_id: SurveyCTO's unique identifier for the form
-			:param date: Date for which the data is to be downloaded, default 0 means all dates
+				Function to get only single repeat group data
+				:param form_id: SurveyCTO's unique identifier for the form
+				:param repeat_group: Repeat group name
 		"""
 
-		if (date != 0):
-			# check date is correct
-			print("to be written")
-		
-		url = f'https://{self.server_name}.surveycto.com/api/v2/forms/data/wide/json/{form_id}?date={date}'
-		data = (get_data_from_url(self, url)).json()
+		review_status_arr = review_status.replace('|', ',').split(',')
+
+		for status in review_status_arr:
+			if (review_status not in ['approved', 'pending', 'rejected']):
+				raise IllegalArgumentError(
+					"Wrong value passed in review_status. Allowed values are approved, rejected and pending.")
+			else:
+				url_review_status=review_status
+
+		url = f'https://{self.server_name}.surveycto.com/api/v1/forms/data/csv/{form_id}/{repeat_group_name}?r={url_review_status}'
+		data = (self.get_url_data(url)).text
 		return data
 
-
-
-	def get_repeatgroup_data(self,
-					  		 form_id,
-					  		 repeat_group):			
-		
+	def get_server_dataset(self,
+						   dataset_id):
 		"""
-			Function to get only single repeat group data
-			:param form_id: SurveyCTO's unique identifier for the form
-			:param repeat_group: Repeat group name
+				Function to get server dataset data
+				:param dataset_id: SurveyCTO's unique identifier for the dataset
 		"""
 
-		url = f'https://{self.server_name}.surveycto.com/api/v1/forms/data/csv/{form_id}/{repeat_group}'
-		data = (get_data_from_url(self, url)).text
-		return data
-
-
-
-	def get_dataset_data(self,
-					  	 dataset_id):	
-		
-		"""
-			Function to get server dataset data
-			:param dataset_id: SurveyCTO's unique identifier for the dataset
-		"""
-		
 		url = f'https://{self.server_name}.surveycto.com/api/v2/datasets/data/csv/{dataset_id}'
-		data = (get_data_from_url(self, url)).text
+		data = (self.get_url_data(url)).text
 		return data
+
+
+	def get_attachment(self,
+					   url):
+		"""
+				Function to get server dataset data
+				:param url: Url to get attachement
+		"""
+
+		data = (self.get_url_data(url)).content
+		return data
+		
