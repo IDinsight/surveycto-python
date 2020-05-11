34862e47a56eba9eddc560a4619fa3ffbd65b96c
jeenut27 <jeenuthomas27@gmail.com>
2020-05-11 13:38:48 +0530

    Correcting package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pysurvecto",
+    name="pysurveycto",
     version="0.0.1",
     author="Eric Dodge, Jeenu Thomas",
     author_email="it@idinsight.org, Eric.Dodge@idinsight.org, Jeenu.Thomas@idinsight.org",
