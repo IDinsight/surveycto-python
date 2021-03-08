f8aaeca6db5a1377408c7104586ee05bb2ee4926
jeenut27 <jeenuthomas27@gmail.com>
2021-03-08 14:54:08 +0530

    Updating version

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 import setuptools
+import os
+import io
 
 dir_path = os.path.abspath(os.path.dirname(__file__))
 long_description = io.open(os.path.join(dir_path, 'README.rst'), encoding='utf-8').read()
 
 setuptools.setup(
     name="pysurveycto",
-    version="0.0.7",
+    version="0.0.8",
     author="Eric Dodge, Jeenu Thomas",
     author_email="it@idinsight.org, Eric.Dodge@idinsight.org, Jeenu.Thomas@idinsight.org",
     description="Interacting with SurveyCTO using Python",
     long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/IDinsight/surveycto-python/",
     packages=setuptools.find_packages(),
     install_requires=[
