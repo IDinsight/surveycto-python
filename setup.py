a07c4dd8b143eae72f74f54aef847c1ee3d6e695
jeenut27 <jeenuthomas27@gmail.com>
2021-04-14 13:45:39 +0530

    Updating version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = io.open(os.path.join(dir_path, 'README.rst'), encoding='utf-8
 
 setuptools.setup(
     name="pysurveycto",
-    version="0.0.9",
+    version="0.0.10",
     author="Eric Dodge, Jeenu Thomas",
     author_email="it@idinsight.org, Eric.Dodge@idinsight.org, Jeenu.Thomas@idinsight.org",
     description="Interacting with SurveyCTO using Python",
